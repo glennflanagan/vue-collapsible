6f2bdc93748e0fcd94da553f8b7f36817c705515
Glenn Flanagan <hello@glenn.codes>
2020-02-19 17:33:14 +0000

    Logic in place. need docs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,156 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Collapsible class="example-collapsible">
+      <!-- <div slot="trigger">
+        Trigger
+      </div>-->
+      <div id="example">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt
+          neque rem at corporis deserunt fugiat? Amet placeat ab commodi culpa.
+          Optio voluptatem aperiam atque aut omnis odio tenetur esse nostrum
+          quod culpa. Consequatur accusamus illum temporibus earum. Laudantium
+          aliquam culpa expedita, animi facere dolorem explicabo quasi
+          excepturi? Illo unde nostrum sint odit officiis molestiae ad omnis
+          dolores suscipit labore, odio laboriosam aut necessitatibus officia
+          itaque? Earum aut eligendi rem placeat aspernatur optio quasi illo
+          culpa iure delectus neque, laudantium architecto magnam ratione
+          tempore est facilis ex sint, amet, et incidunt nobis similique vero!
+          Nam, itaque, aliquam neque perferendis dolores assumenda doloremque
+          perspiciatis necessitatibus odio fugiat minus? Maiores, perspiciatis?
+          Voluptate consectetur nemo temporibus adipisci quasi corporis,
+          eveniet, ullam atque recusandae facere minus animi maxime tempore quis
+          ipsam cum nihil harum dolorum amet. Expedita commodi consequatur
+          reprehenderit porro libero quidem velit, harum in, possimus maiores
+          culpa, nemo exercitationem accusantium temporibus? Voluptates vero
+          eligendi, fugiat eos nostrum repudiandae ex fuga pariatur accusamus
+          nemo dolore provident at doloremque rem veniam a aut sed excepturi!
+          Ipsum illo quae, quidem deserunt possimus eius tempora facere maxime
+          voluptate in maiores molestiae similique quos nulla inventore
+          quibusdam praesentium molestias aliquam labore ut odit quod
+          voluptatibus eum accusamus.
+        </p>
+      </div>
+    </Collapsible>
+
+    <Collapsible
+      class="example-collapsible"
+      openLabel="Custom open label"
+      closedLabel="Custom closed label"
+      :isOpen="false"
+    >
+      <div id="example2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt
+          neque rem at corporis deserunt fugiat? Amet placeat ab commodi culpa.
+          Optio voluptatem aperiam atque aut omnis odio tenetur esse nostrum
+          quod culpa. Consequatur accusamus illum temporibus earum. Laudantium
+          aliquam culpa expedita, animi facere dolorem explicabo quasi
+          excepturi? Illo unde nostrum sint odit officiis molestiae ad omnis
+          dolores suscipit labore, odio laboriosam aut necessitatibus officia
+          itaque? Earum aut eligendi rem placeat aspernatur optio quasi illo
+          culpa iure delectus neque, laudantium architecto magnam ratione
+          tempore est facilis ex sint, amepora facere maxime voluptate in
+          maiores molestiae similique quos nulla inventore quibusdam praesentium
+          molestias aliquam labore ut odit quod voluptatibus eum accusamus.
+        </p>
+      </div>
+    </Collapsible>
+
+    <Collapsible class="example-collapsible">
+      <div slot="trigger">
+        <div class="customTrigger">
+          <h2>Custom open trigger element</h2>
+          <Icon />
+        </div>
+      </div>
+
+      <div slot="closedTrigger">
+        <div class="customTrigger">
+          <h2>Custom closed trigger element</h2>
+          <Icon />
+        </div>
+      </div>
+
+      <div id="example2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt
+          neque rem at corporis deserunt fugiat? Amet placeat ab commodi culpa.
+          Optio voluptatem aperiam atque aut omnis odio tenetur esse nostrum
+          quod culpa. Consequatur accusamus illum temporibus earum. Laudantium
+          aliquam culpa expedita, animi facere dolorem explicabo quasi
+          excepturi? Illo unde nostrum sint odit officiis molestiae ad omnis
+          dolores suscipit labore, odio laboriosam aut necessitatibus officia
+          itaque? Earum aut eligendi rem placeat aspernatur optio quasi illo
+          culpa iure delectus neque, laudantium architecto magnam ratione
+          tempore est facilis ex sint, amepora facere maxime voluptate in
+          maiores molestiae similique quos nulla inventore quibusdam praesentium
+          molestias aliquam labore ut odit quod voluptatibus eum accusamus.
+        </p>
+      </div>
+    </Collapsible>
+
+    <button @click="collapsibleIsOpen = !collapsibleIsOpen">Trigger collapsible prgramatically</button>
+
+    <Collapsible
+      class="example-collapsible"
+      :isOpen="collapsibleIsOpen"
+      :onCollapse="handleCollapse"
+    >
+      <div slot="trigger">
+        <div class="customTrigger">
+          <h2>Custom open trigger element</h2>
+          <Icon />
+        </div>
+      </div>
+
+      <div slot="closedTrigger">
+        <div class="customTrigger">
+          <h2>Custom closed trigger element</h2>
+          <Icon />
+        </div>
+      </div>
+
+      <div id="example2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt
+          neque rem at corporis deserunt fugiat? Amet placeat ab commodi culpa.
+          Optio voluptatem aperiam atque aut omnis odio tenetur esse nostrum
+          quod culpa. Consequatur accusamus illum temporibus earum. Laudantium
+          aliquam culpa expedita, animi facere dolorem explicabo quasi
+          excepturi? Illo unde nostrum sint odit officiis molestiae ad omnis
+          dolores suscipit labore, odio laboriosam aut necessitatibus officia
+          itaque? Earum aut eligendi rem placeat aspernatur optio quasi illo
+          culpa iure delectus neque, laudantium architecto magnam ratione
+          tempore est facilis ex sint, amepora facere maxime voluptate in
+          maiores molestiae similique quos nulla inventore quibusdam praesentium
+          molestias aliquam labore ut odit quod voluptatibus eum accusamus.
+        </p>
+      </div>
+    </Collapsible>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Collapsible from './components/Collapsible.vue';
+import Icon from './components/Icon';
 
 export default {
-  name: "App",
+  name: 'App',
+  data() {
+    return {
+      collapsibleIsOpen: false,
+    };
+  },
+  methods: {
+    handleCollapse(open) {
+      this.collapsibleIsOpen = open;
+    },
+  },
   components: {
-    HelloWorld
-  }
+    Collapsible,
+    Icon,
+  },
 };
 </script>
 
@@ -21,8 +159,59 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+.example-collapsible {
+  margin: 30px 0;
+}
+
+.example-collapsible .Collapsible__contentInner {
+  padding: 20px;
+}
+
+.example-collapsible .Collapsible__trigger {
+  padding: 10px 20px;
+  border: 2px solid aquamarine;
+}
+.example-collapsible .Collapsible__trigger {
+  padding: 10px 20px;
+  border: 2px solid aquamarine;
+}
+
+.example-collapsible .Collapsible__content {
+  border: 2px solid aquamarine;
+  border-top: 0;
+}
+
+.example-collapsible .Collapsible__content--closed {
+  border: 0;
+}
+
+.customTrigger {
+  display: grid;
+  grid-template-columns: auto 20px;
+}
+
+.customTrigger svg {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.customTrigger h2 {
+  margin: 0;
+  font-size: 16px;
 }
 </style>
---
+++ src/components/Collapsible.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="Collapsible">
+    <button
+      @click="handleClick"
+      :class="{
+        Collapsible__trigger: true,
+        'Collapsible__trigger--open': !transitioning && open,
+        'Collapsible__trigger--closed': !transitioning && !open,
+        'Collapsible__trigger--transitioning': transitioning,
+      }"
+    >
+      <slot name="trigger" v-if="open">
+        <span class="Collapsible__triggerLabel">{{ !open ? closedLabel : openLabel }}</span>
+      </slot>
+
+      <slot name="closedTrigger" v-if="!open">
+        <slot name="trigger">
+          <span class="Collapsible__triggerLabel">
+            {{
+            !open ? closedLabel : openLabel
+            }}
+          </span>
+        </slot>
+      </slot>
+    </button>
+
+    <div
+      :class="{
+        Collapsible__content: true,
+        'Collapsible__content--open': !transitioning && open,
+        'Collapsible__content--closed': !transitioning && !open,
+        'Collapsible__content--transitioning': transitioning,
+      }"
+      :style="{
+        height,
+        transitionProperty: 'height',
+        transitionDuration,
+        transitionTimingFunction,
+        transitionDelay,
+      }"
+      @transitionend="handleEnd"
+    >
+      <div class="Collapsible__contentInner" ref="inner">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Collapsible',
+  data() {
+    return {
+      open: this.isOpen,
+      height: this.isOpen ? 'auto' : '0px',
+      closeOnNextTick: false,
+      transitioning: false,
+    };
+  },
+  props: {
+    openLabel: {
+      type: String,
+      default: 'Open me',
+    },
+    closedLabel: {
+      type: String,
+      default: 'Close me',
+    },
+    transitionDuration: {
+      type: String,
+      default: '400ms',
+    },
+    transitionTimingFunction: {
+      type: String,
+      default: 'ease',
+    },
+    transitionDelay: {
+      type: String,
+      default: '0s',
+    },
+    isOpen: {
+      default: true,
+      type: Boolean,
+    },
+    onCollapse: {
+      default: () => {},
+      type: Function,
+    },
+  },
+  methods: {
+    handleClick() {
+      // this.transitioning = true;
+      // this.height = this.$refs.inner.scrollHeight + 'px';
+      this.onCollapse(!this.isOpen);
+      this.open = !this.open;
+      // if (this.open) {
+      //   this.closeOnNextTick = true;
+      //   this.open = false;
+      // } else {
+      // }
+    },
+    handleEnd() {
+      if (this.height !== '0px') {
+        this.height = 'auto';
+      }
+      this.transitioning = false;
+    },
+  },
+  watch: {
+    open(newVal, oldVal) {
+      this.transitioning = true;
+      this.height = this.$refs.inner.scrollHeight + 'px';
+
+      if (oldVal === true) {
+        this.closeOnNextTick = true;
+      }
+    },
+    isOpen(newVal) {
+      this.open = newVal;
+    },
+  },
+  updated() {
+    this.$nextTick(() => {
+      window.setTimeout(() => {
+        if (this.closeOnNextTick) {
+          this.height = '0px';
+          this.closeOnNextTick = false;
+        }
+      });
+    });
+  },
+};
+</script>
+
+<style scoped>
+.Collapsible__content {
+  overflow: hidden;
+}
+
+.Collapsible__contentInner {
+  height: auto;
+}
+
+.Collapsible__trigger {
+  appearance: none;
+  border: 0;
+  background: transparent;
+  border-radius: 0;
+  font-family: inherit;
+  font-size: inherit;
+  font-style: inherit;
+  text-align: inherit;
+  color: inherit;
+  padding: 0;
+  margin: 0;
+  display: block;
+  width: 100%;
+  cursor: pointer;
+}
+
+.Collapsible__trigger.Collapsible__trigger--open svg {
+  transform: rotate(0.5turn);
+}
+</style>
